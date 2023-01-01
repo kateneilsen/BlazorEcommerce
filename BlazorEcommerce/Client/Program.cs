67591fe625c11f47a07fd5c96ce50fd83ff9e613
Kate Neilsen <neilsenk09@gmail.com>
2022-12-31 19:54:04 -0500

    added ProductService to the client

--- BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/Client/Program.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.AspNetCore.Components.Web;
+﻿global using BlazorEcommerce.Shared;
+global using System.Net.Http.Json;
+global using BlazorEcommerce.Client.Services.ProductService;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorEcommerce.Client;
+using BlazorEcommerce.Client.Services.ProductService;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services.AddScoped<IProductService, ProductService>();
 await builder.Build().RunAsync();
 
