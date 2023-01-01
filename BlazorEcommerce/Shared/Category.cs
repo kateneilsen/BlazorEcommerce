37543f7666cf40729093669adbc9365ebc585b56
Kate Neilsen <neilsenk09@gmail.com>
2023-01-01 12:26:49 -0500

    added category seeding and migration

--- BlazorEcommerce/Shared/Category.cs
+++ BlazorEcommerce/Shared/Category.cs
@@ -2,6 +2,17 @@ namespace BlazorEcommerce.Shared;
 
 public class Category
 {
+    public Category()
+    {
+    }
+
+    public Category(int id, string name, string url)
+    {
+        Id = id;
+        Name = name;
+        Url = url;
+    }
+
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Url { get; set; } = string.Empty;
